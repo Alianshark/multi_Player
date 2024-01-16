05fda71dd11a890bc66c97005433a57f7e99d087
Dimon Cheek <dimon.sergiienko@gmail.com>
2024-01-17 00:11:02 +0200

    refactor frontEnd.js

--- frontEnd.js
+++ frontEnd.js
@@ -4,7 +4,7 @@ let players = []
 socket.onopen = function(e) {
     socket.send(JSON.stringify(player));
 };
-  
+
 socket.onmessage = function(client) {
     document.body.innerHTML = ""
     let updatedPlayers = JSON.parse(client.data)
@@ -15,7 +15,7 @@ socket.onmessage = function(client) {
         img.style.left = newPlayer.x + 'px'
         img.style.top = newPlayer.y + 'px'
         document.body.append(img)
-    });    
+    });
 }
 
 let player = {
@@ -23,40 +23,23 @@ let player = {
     y: 50,
 }
 
-function keyboardMoving() {
- addEventListener('keydown', leftMove)
- addEventListener('keydown', rightMove)
- addEventListener('keydown', upMove)
- addEventListener('keydown', downMove)
-}
-
-keyboardMoving()
-
-function leftMove(event) {
+function playerMovement(event) {
     if (event.key == 'ArrowLeft') {
         player.x -= 10
         socket.send(JSON.stringify(player));
     }
-}
-
-function rightMove(event) {
     if (event.key == 'ArrowRight') {
         player.x += 10
         socket.send(JSON.stringify(player));
     }
-}
-
-function upMove(event) {
-    
     if (event.key == 'ArrowUp') {
         player.y -= 10
         socket.send(JSON.stringify(player));
     }
-}
-
-function downMove(event) {
     if (event.key == 'ArrowDown') {
         player.y += 10
         socket.send(JSON.stringify(player));
     }
 }
+
+addEventListener('keydown', playerMovement)
