5a980c3faaa9dcad590f7b4d263f96f4e82daf12
EnormouseJohnn <109089482+Alianshark@users.noreply.github.com>
2024-01-16 19:21:53 +0200

    add movement for img

--- frontEnd.js
+++ frontEnd.js
@@ -1,25 +1,84 @@
 let socket = new WebSocket("ws://localhost:8080");
 
+
 socket.onopen = function(e) {
-    alert("[open] З’єднання встановлено");
-    alert("Відправка на сервер");
+    //document.body.innerHTML += "[open] З’єднання встановлено"
+    // alert("Відправка на сервер");
+   // document.body.innerHTML += "Відправка на сервер"
     socket.send("Мене звати Джон");
   };
   
   socket.onmessage = function(event) {
-    alert(`[message] Дані отримані із сервера: ${event.data}`);
+    //document.body.innerHTML += `<div>${event.data}</div>`
+    //alert(`[message] Дані отримані із сервера: ${event.data}`);
   };
   
-  socket.onclose = function(event) {
+  /*socket.onclose = function(event) {
     if (event.wasClean) {
-      alert(`[close] З’єднання закрите чисто, код=${event.code} причина=${event.reason}`);
+     // alert(`[close] З’єднання закрите чисто, код=${event.code} причина=${event.reason}`);
     } else {
       // наприклад сервер завершив процес або мережа не працює
       // у цьому випадку event.code зазвичай дорівнює 1006
       alert('[close] З’єднання перервано');
     }
-  };
+  }; */
   
   socket.onerror = function(error) {
     alert(`[error]`);
   };
+let img = document.createElement('img')
+let x = 50 
+let y = 50
+img.src = '/vintage-robot-toy-white-background_1308-77501.avif'
+img.style.position = 'absolute'
+img.style.left = 200 + 'px'
+document.body.append(img)
+
+
+function position() {
+    img.style.left = x + 'px'
+    img.style.top = y + 'px'
+}
+
+function keyboardMoving() {
+ addEventListener('keydown', leftMove)
+ addEventListener('keydown', rightMove)
+ addEventListener('keydown', upMove)
+ addEventListener('keydown', downMove)
+
+}
+keyboardMoving()
+
+function leftMove(event) {
+    
+    if (event.key == 'ArrowLeft') {
+        console.log('ArrowLeft Pressed')
+        x -= 10
+    }
+}
+function rightMove(event) {
+    if (event.key == 'ArrowRight') {
+        console.log('ArrowRight Pressed')
+        x += 10
+    }
+}
+function upMove(event) {
+    
+    if (event.key == 'ArrowUp') {
+        console.log('ArrowUp Pressed')
+        y -= 10
+    }
+}
+function downMove(event) {
+    if (event.key == 'ArrowDown') {
+        console.log('Arrowdown Pressed')
+        y += 10
+    }
+}
+setInterval(imgStep,10)     
+
+function imgStep () {
+    console.log('what?')
+    position()
+    
+}
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-import { WebSocketServer } from 'ws';
+import WebSocket, { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
-
+//setInterval(connectionTimer,1000)
+function connectionTimer() {
+    wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(`Timer: ${new Date()}`);
+    }
+  });
+    
+}
 wss.on('connection', function connection(ws) {
   console.log('client connected');
-ws.on('error', console.error);
+  //console.log(wss.clients, 'wss clients');
+  
+  ws.on('error', console.error);
 
   ws.on('message', function message(data) {
     console.log('received: %s', data);
   });
 
-  ws.send('something');
+  ws.send('something new');
 
 });
